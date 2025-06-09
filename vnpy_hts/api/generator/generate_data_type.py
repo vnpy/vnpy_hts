a58344fa212ee89f5308b0163a6fd938a33d9258
noranhe <63988546+noranhe@users.noreply.github.com>
2025-06-09 17:56:44 +0800

    [Mod] 基于mypy优化代码质量

--- vnpy_hts/api/generator/generate_data_type.py
+++ vnpy_hts/api/generator/generate_data_type.py
@@ -34,7 +34,7 @@ class DataTypeGenerator:
         self.filename = filename
         self.prefix = prefix
 
-    def run(self):
+    def run(self) -> None:
         """主函数"""
         self.f_cpp = open(self.filename, encoding="UTF-8")
         self.f_define = open(f"{self.prefix}_constant.py", "w")
@@ -49,7 +49,7 @@ class DataTypeGenerator:
 
         print("DataType生成完毕")
 
-    def process_line(self, line: str):
+    def process_line(self, line: str) -> None:
         """处理每行"""
         line = line.replace("\n", "")
         line = line.replace(";", "")
@@ -59,7 +59,7 @@ class DataTypeGenerator:
         elif line.startswith("typedef"):
             self.process_typedef(line)
 
-    def process_define(self, line: str):
+    def process_define(self, line: str) -> None:
         """处理常量定义"""
         words = line.split(" ")
         words = [word for word in words if word]
@@ -72,7 +72,7 @@ class DataTypeGenerator:
         new_line = f"{name} = {value}\n"
         self.f_define.write(new_line)
 
-    def process_typedef(self, line: str):
+    def process_typedef(self, line: str) -> None:
         """处理类型定义"""
         words = line.split(" ")
         words = [word for word in words if word != ""]
